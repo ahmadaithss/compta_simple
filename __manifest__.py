84d26e146c6f85741cb40c6ab7579d6f4b16cb0a
root <root@localhost.localdomain>
2017-04-17 13:35:55 +0100

    form d'ajout d'une transaction:categorie,sous categorie

--- __manifest__.py
+++ __manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product','account'],
+    'depends': ['base','account'],
 
     # always loaded
     'data': [
--- models/config_transactions.py
+++ models/config_transactions.py
@@ -2,14 +2,35 @@
 from odoo import api, fields, models, _
 class transaction_achat_category(models.Model):
     _inherit = 'product.category'
-    _name = "transactionconfig.category"
-
+    _name = "transaction.config.category"
+    compte_debit = fields.Many2one('account.account',
+                                   string="compte de debit",
+                                   required=True,
+                                   help="le compte de débit qui sera utilisé lors de la génération de l'écriture comptable de la transaction.")
+    parent_id = fields.Many2one('transaction.config.category', 'Parent Category', index=True, ondelete='cascade')
+    child_id = fields.One2many('transaction.config.category', 'parent_id', 'Child Categories')
 
 class transaction_achat_config(models.Model):
     _inherit = 'product.template'
-    _name="transactioncofig.achat"
+    _name="transaction.config.achat"
     #les tva applicable sur une transactions
     tax_ids = fields.Many2many('account.tax', 'transaction_achat_config_tva_default_rel',
-                                   'transaction_achat_config_id', 'tax_id', string='TVA par défaut')
+                                   'transaction_achat_config_id', 'tax_id', string='TVA par défaut',
+                                    domain=[('type_tax_use', '=', 'purchase')],required=True)
+
+    #définir la valeur par défaut a true parceque il s'agit d'un achat
     purchase_ok = fields.Boolean('transaction d\'achat', default=True)
+
+    # définir la valeur par défaut a false il ne peut pas etre un achat parceque a ce stade on traite seulment les achats
     sale_ok = fields.Boolean('transaction de vente',defaut=False)
+
+    #le compte de debit de la transaction
+    compte_debit=fields.Many2one('account.account',
+        string="compte de debit",
+        required=True,
+        help="le compte de débit qui sera utilisé lors de la génération de l'écriture comptable de la transaction.")
+
+    #les categories des transactions
+    categorie=fields.Many2one(
+        'transaction.config.category', 'catégorie',
+        required=True, help="Selectionner la categorie de la transaction")
