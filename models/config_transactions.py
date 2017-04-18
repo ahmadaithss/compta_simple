4ea40d7bdaede4eae422d76ed4479f377984cf8a
root <root@localhost.localdomain>
2017-04-18 12:43:53 +0100

    add fournisseur

--- models/config_transactions.py
+++ models/config_transactions.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
+
+#configuration des categorie des transactions
 class transaction_achat_category(models.Model):
     _inherit = 'product.category'
     _name = "transaction.config.category"
     compte_debit = fields.Many2one('account.account',
                                    string="compte de debit",
-                                   required=True,
                                    help="le compte de débit qui sera utilisé lors de la génération de l'écriture comptable de la transaction.")
     parent_id = fields.Many2one('transaction.config.category', 'Parent Category', index=True, ondelete='cascade')
     child_id = fields.One2many('transaction.config.category', 'parent_id', 'Child Categories')
 
+#configuration des transaction d'achats
 class transaction_achat_config(models.Model):
     _inherit = 'product.template'
     _name="transaction.config.achat"
     #les tva applicable sur une transactions
     tax_ids = fields.Many2many('account.tax', 'transaction_achat_config_tva_default_rel',
                                    'transaction_achat_config_id', 'tax_id', string='TVA par défaut',
-                                    domain=[('type_tax_use', '=', 'purchase')],required=True)
+                                    domain=[('type_tax_use', '=', 'purchase')])
 
     #définir la valeur par défaut a true parceque il s'agit d'un achat
     purchase_ok = fields.Boolean('transaction d\'achat', default=True)
@@ -26,11 +28,15 @@ class transaction_achat_config(models.Model):
 
     #le compte de debit de la transaction
     compte_debit=fields.Many2one('account.account',
-        string="compte de debit",
+        string="Compte de debit",
         required=True,
         help="le compte de débit qui sera utilisé lors de la génération de l'écriture comptable de la transaction.")
 
     #les categories des transactions
     categorie=fields.Many2one(
-        'transaction.config.category', 'catégorie',
+        'transaction.config.category', 'Catégorie',
         required=True, help="Selectionner la categorie de la transaction")
+       #une transaction peut avoir plusieurs fournisseur
+    partner_ids=fields.Many2many('res.partner','transaction_achat_config_partner_rel',
+                                 'transaction_achat_config_id','partner_id',string='La liste des fournisseurs',
+                                  domaine=[('supplier'),'=',True])
---
+++ models/partner.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from odoo import fields,models,api, _
+class Partner(models.Model):
+    _name="res.partner"
+    _inherit = "res.partner"
